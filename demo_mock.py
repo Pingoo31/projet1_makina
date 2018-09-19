d7b2daf51e759652132e04f43e3dab251472f894
Stéphane THIPHONET <stephane.thiphonet@aviation-civile.gouv.fr>
2018-09-19 14:25:59 +0200

    ajout requirements.txt

--- demo_mock.py
+++ demo_mock.py
@@ -1,15 +1,16 @@
+import unittest
+
 import requests
+import responses
 from prettyprinter import cpprint
 
 from src.user import User
 
 
-class APIUnreachableException(Exception):
-    pass
+class APIUnreachableException(Exception): pass
 
 
-class HttpNotFound(Exception):
-    pass
+class HttpNotFound(Exception): pass
 
 
 def get_user():
@@ -40,4 +41,42 @@ def main():
         print("l'URL n'exite pas")
 
 
-main()
+mock_user = {
+    'results': [{
+        'email': 'stephane@thipho.net',
+        'name': {
+            'title': 'Mr',
+            'first': 'Stéphane',
+            'last': 'THIPHONET',
+        },
+        'login': {
+            'username': 'Pingoo'
+        },
+        'gender': 'male',
+
+    }]
+}
+
+
+class Test_user(unittest.TestCase):
+
+    @responses.activate
+    def test_get_user(self):
+        responses.add(responses.GET, 'http://randomuser.me/api/', json=mock_user)
+        user = get_user()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.prenom, "Stéphane")
+        self.assertEqual(user.nom, "THIPHONET")
+
+    # @unittest.skip("In progress")
+    @responses.activate
+    def test_get_user_server_unreachable(self):
+        with self.assertRaises(APIUnreachableException):
+            user = get_user()
+
+    # @unittest.skip("In progress")
+    @responses.activate
+    def test_get_user_url_not_exist(self):
+        responses.add(responses.GET, 'http://randomuser.me/api/', status=404)
+        with self.assertRaises(HttpNotFound):
+            user = get_user()
