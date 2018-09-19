d68ee80eaf224a092ee4febf0792c1f5c9918655
Stéphane THIPHONET <stephane.thiphonet@aviation-civile.gouv.fr>
2018-09-19 10:38:05 +0200

    demo mock

--- demo_mock.py
+++ demo_mock.py
@@ -1,14 +1,43 @@
 import requests
+from prettyprinter import cpprint
 
 from src.user import User
 
-response = requests.get('http://randomuser.me/api/')
 
-result = response.json()
-# prettyprinter.cpprint(result)
-record = result['results'][0]
+class APIUnreachableException(Exception):
+    pass
 
-user = User(record['name']['title'], record['name']['last'], record['name']['first'], record['login']['username'],
-            record['email'])
 
-print(f"{user.titre} {user.nom_complet}")
+class HttpNotFound(Exception):
+    pass
+
+
+def get_user():
+    user = None
+    try:
+        response = requests.get('http://randomuser.me/api/')
+        if response.status_code == 404:
+            raise HttpNotFound()
+        result = response.json()
+        # prettyprinter.cpprint(result)
+        record = result['results'][0]
+
+        return User(record['name']['title'], record['name']['last'], record['name']['first'],
+                    record['login']['username'],
+                    record['email'])
+    except requests.exceptions.ConnectionError:
+        raise APIUnreachableException()
+
+
+def main():
+    try:
+        user = get_user()
+        cpprint(user)
+        # print(f"{user.titre} {user.nom_complet}")
+    except APIUnreachableException:
+        print("L'API est injoignable.")
+    except HttpNotFound:
+        print("l'URL n'exite pas")
+
+
+main()
